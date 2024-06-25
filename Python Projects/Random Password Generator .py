4756ec0d881ca108e1eb1966ce6b25d36070c833
Prottay <fahimulhaq2001@gmail.com>
2024-06-25 21:50:50 +0600

    Update and rename Random Pass generator .py to Random Password Generator .py

--- Python Projects/Random Password Generator .py
+++ Python Projects/Random Password Generator .py
@@ -1,3 +1,4 @@
+#This project creates a random password and the user decides the number of characters
 import random
 
 letter=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
